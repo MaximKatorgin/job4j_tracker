3bf5030fd21fd4b67131e75eafae4529a61a08ca
Katorgin Maxim <KatorginMI@yandex.ru>
2026-02-11 09:40:56 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -38,5 +31,26 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(result, resultSize);
+    }
+
+    public boolean replace(int id, Item item) {
+        int indexForReplacement = indexOf(id);
+        if (indexForReplacement == -1) {
+            return false;
+        }
+        item.setId(items[indexForReplacement].getId());
+        items[indexForReplacement] = item;
+        return true;
+    }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                result = index;
+                break;
+            }
+        }
+        return result;
     }
 }
