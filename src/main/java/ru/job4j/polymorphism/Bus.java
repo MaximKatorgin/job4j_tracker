e010b989082057d2b558eb8781d7e37e6ae67d93
Katorgin Maxim <KatorginMI@yandex.ru>
2026-02-19 18:38:33 +0300

    4.2. Интерфейс Input [#181092 #558410]

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -1,16 +1,20 @@
 package ru.job4j.polymorphism;
 
 public class Bus implements Transport {
+    private int fuel = 0;
+
     @Override
     public void move() {
+        System.out.println("Автобус поехал");
     }
 
     @Override
     public void passengers() {
+        System.out.println("Пересчитываем пассажиров");
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        return this.fuel + fuel;
     }
 }
