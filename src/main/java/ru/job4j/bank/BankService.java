5a13b8d0e667b538e68e888860755df98d9cc427
Katorgin Maxim <KatorginMI@yandex.ru>
2026-03-02 18:59:27 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,77 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void deleteUser(String passport) {
+        users.remove(new User(passport, null));
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = this.findByPassport(passport);
+        if (user != null) {
+            List<Account> userAccounts = users.get(user);
+            if (!userAccounts.contains(account)) {
+                userAccounts.add(account);
+            }
+        }
+
+    }
+
+    public User findByPassport(String passport) {
+        User result = null;
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account result = null;
+        User user = this.findByPassport(passport);
+        if (user != null) {
+            List<Account> userAccounts = users.get(user);
+            for (Account account : userAccounts) {
+                if (account.getRequisite().equals(requisite)) {
+                    result = account;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean transferMoney(String sourcePassport, String sourceRequisite,
+                                 String destinationPassport, String destinationRequisite,
+                                 double amount) {
+        boolean result = false;
+        Account sourceAccount = this.findByRequisite(sourcePassport, sourceRequisite);
+        Account destinationAccount = this.findByRequisite(destinationPassport, destinationRequisite);
+        if (sourceAccount == null || destinationAccount == null) {
+            return result;
+        }
+        if (sourceAccount.getBalance() < amount) {
+            return result;
+        }
+        sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+        destinationAccount.setBalance(destinationAccount.getBalance() + amount);
+        result = true;
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
